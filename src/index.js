5d79171b45cc78db3499ac324938f2dd77606b41
Akshith <akshithbharadwaj@gmail.com>
2020-06-09 09:47:19 +0530

    React Components Part-1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+ReactDOM.render( //calling the render method of ReactDOM
   <React.StrictMode>
     <App />
+    {/*app is the parameter (imported from above)*/}
   </React.StrictMode>,
   document.getElementById('root')
+    //It basically says,
+    //ReactDOM.render this particular thing (App), and then attach it to this DOM element in my index.html page.
 );
 
 // If you want your app to work offline and load faster, you can change
