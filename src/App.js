cee27cad743c44e5d8bd17b3aed5192ffae6b64e
Akshith <akshithbharadwaj@gmail.com>
2020-06-09 12:03:30 +0530

    React Components Part-2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Navbar, NavbarBrand} from "reactstrap"; //react implementation of bootstrap
+import React, { Component } from "react";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap"; //react implementation of bootstrap
 import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes"; //importing the const
 
 class App extends Component {
+  constructor(props) {
+    //we need to define state in the constructor
+    super(props);
+
+    this.state = {
+      //we can simply use the DISHES const in the state instead of writing it here
+      dishes: DISHES,
+    };
+  }
   render() {
     return (
-       <div >
-       <Navbar dark color="primary">
-           <div className="container">
-            <NavbarBrand href="/">
-            Ristorante Con Fusion
-            </NavbarBrand>
-           </div>
-       </Navbar>
-           <Menu/>
-       </div>
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} /> {/*making dishes available as props to the menu component*/}
+        
+      </div>
     );
   }
 }
