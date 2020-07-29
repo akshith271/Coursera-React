08041c822ce39fa1c799401d9fffd51258374d33
Akshith <akshithbharadwaj@gmail.com>
2020-07-29 06:23:49 +0530

    React Animation Components

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -9,7 +9,11 @@ import {
 } from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform } from 'react-animation-components';
 
+/**
+
+ */
 function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
         return <Loading />;
@@ -18,16 +22,23 @@ function RenderCard({ item, isLoading, errMess }) {
     } else {
         if (!item) return null;
         return (
-            <Card>
-                <CardImg src={baseUrl + item.image} alt={item.name} />
-                <CardBody>
-                    <CardTitle>{item.name}</CardTitle>
-                    {item.designation ? (
-                        <CardSubtitle>{item.designation}</CardSubtitle>
-                    ) : null}
-                    <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)',
+                }}
+            >
+                <Card>
+                    <CardImg src={baseUrl + item.image} alt={item.name} />
+                    <CardBody>
+                        <CardTitle>{item.name}</CardTitle>
+                        {item.designation ? (
+                            <CardSubtitle>{item.designation}</CardSubtitle>
+                        ) : null}
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         );
     }
 }
